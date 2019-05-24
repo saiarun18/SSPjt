0c82d62f39db1de2d80160a40ea709ea5ff9d144
50023196 <50023196@CHNIDC0877.acsind.com>
2019-05-24 07:23:03 +0530

    SSpjt commit

---
+++ src/main/java/bl/framework/api/demo1.java
@@ -0,0 +1,21 @@
+package bl.framework.api;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo1 extends SeleniumBase{
+
+  	public void test1()
+  	{
+  		
+  		locateElement("name", "namevalue");
+  		
+  	}
+	
+		
+		
+		
+
+	}
+
+
